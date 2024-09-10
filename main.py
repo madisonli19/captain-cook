a7c1fb6b1305e65951de77193ca3232943a17436
Madi Little <mrl93@pitt.edu>
2024-09-10 11:18:43 -0400

    Main driver upload + starting dialogue

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+main.py - driver for basic text execution of Captain Cook prototype
+Author: Madison Little
+Last Updated: 8/16/2024
+'''
+import random
+import pandas as pd
+
+if __name__ == "__main__":
+    # TODO: Check if player data file exists
+    print(''' 
+        ####################
+        ##  CAPTAIN COOK  ##
+        ####################
+        By Madison Little, 2024
+        ''')
+    
+    ## DIALOGUE ##
+    print("Captain Cook: AHOY Chef! Ready to spin the wheels?\n")
+    input("/Press Enter/\n")
+    print("Captain Cook: Aye, the galley is at the ready. " +
+          "Let's see which ingredients aren't rotten today.\n")
+    
